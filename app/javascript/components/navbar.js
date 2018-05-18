7bd4c51148279908c2fc31b5fae0620558f3e5ba
morgan krischer <morgan.krischer@gmail.com>
2018-05-18 13:28:00 +0100

    reorganising grid in show

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -1,7 +1,7 @@
 function initUpdateNavbarOnScroll() {
   const navbar = document.querySelector('.navbar-wagon');
   window.addEventListener('scroll', () => {
-    if (window.scrollY >= (0.9*window.innerHeight)) {
+    if (window.scrollY >= window.innerHeight) {
       navbar.classList.add('navbar-wagon-white');
     } else {
       navbar.classList.remove('navbar-wagon-white');
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :cocktails, only: [:index, :new, :create, :show] do
+  root to: 'cocktails#index'
+  resources :cocktails, only: [:new, :create, :show] do
     resources :doses, only: [:create, :destroy ]
   end
 end
